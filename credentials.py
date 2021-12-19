333f43160801bf5011406ee647ddc1877ab3575b
bernd-mack <76614620+bernd-mack@users.noreply.github.com>
2021-12-19 16:20:55 +0100

    extract configurations ans password/usernames

---
+++ credentials.py
@@ -0,0 +1,4 @@
+WWW_DIR = "c:/temp/api_collector/www"
+API_LIST = "c:/Temp/api_collector/api_calls.txt"
+DEFICONF = "c:/users/bmack/.defi/defi.conf"
+DEFID = "C:/Program Files/DeFi Wallet/resources/binary/win/defid.exe"
