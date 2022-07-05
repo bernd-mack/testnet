9651ca6943bfa57b7caaa104ffc8aae44f14b14d
bernd-mack <76614620+bernd-mack@users.noreply.github.com>
2022-07-05 18:07:15 +0200

    pretty and add telegraminformation

--- credentials.py
+++ credentials.py
@@ -1,4 +1,8 @@
-WWW_DIR = "c:/temp/api_collector/www"
-API_LIST = "c:/Temp/api_collector/api_calls.txt"
-DEFICONF = "c:/users/bmack/.defi/defi.conf"
-DEFID = "C:/Program Files/DeFi Wallet/resources/binary/win/defid.exe"
+WWW_DIR  = "c:/api_collector/www"
+API_LIST = "c:/api_collector/api_calls.txt"
+DEFICONF = "c:/users/username/.defi/defi.conf"
+DEFID    = "C:/Program Files/DeFi Wallet/resources/binary/win/defid.exe"
+
+TELEGRAM_TOKEN           = "bottoken" #request from telegram botfather
+TELEGRAM_CHATID_ALARM    = "-123456789" #Alarmchannel
+TELEGRAM_CHATID_STATUS   = "-123456789" #Statuschannel
