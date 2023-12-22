ac2f3c1c44b316cfb64587f80665146954ee2a58
bernd-mack <76614620+bernd-mack@users.noreply.github.com>
2023-12-22 20:59:06 +0100

    update to json

--- api_collector.py
+++ api_collector.py
@@ -1,37 +1,56 @@
 from os import path
-from requests import get
 from logging import basicConfig, info, INFO
-from credentials import WWW_DIR, DEFID, DEFICONF, API_LIST, TELEGRAM_TOKEN, TELEGRAM_CHATID_ALARM
-from subfunctions import save_json_to_www, create_connection_rpc, get_systeminfo, get_version
-from subfunctions import read_deficonf, remove_unused_dirs, api_calls, get_operators, get_servername
-
-# hint: psutil, shutil, python-bitcoinrpc, requests not a standard python lib
+from credentials import WWW_DIR, DEFID, DEFICONF, API_LIST, TELEGRAM_TOKEN, TELEGRAM_CHATID_ALARM, TELEGRAM_CHATID_STATUS, NETWORK
+from subfunctions import save_json_to_www, get_systeminfo, get_version, get_credentials_from_config
+from subfunctions import remove_unused_dirs, api_calls, get_operators, get_servername
+from json import dumps
+from datetime import datetime
 
 # variables
+starttime = datetime.now()
 filename = path.basename(__file__)
 directory = path.dirname(__file__)
 logfile = path.dirname(__file__)+"/"+filename.split(".")[0]+".log"
+servername, ip_address = get_servername()
 
 # program start
 basicConfig(filename=logfile, format='%(asctime)s - %(message)s', level=INFO)
 info("######################################")
 info(f"Start {filename}")
 
-errors = []
+cred = get_credentials_from_config(DEFICONF, network=NETWORK)
+
+url = f'http://{cred["rpcbind"]}:{cred["rpcport"]}'
+
+def rpc(method, params=[]):
+    payload = dumps({
+        "jsonrpc": "2.0",
+        "id": " mydefichain",
+        "method": method,
+        "params": params
+    })
+    response = post(url, auth=(cred["rpcuser"], cred["rpcpass"]), data=payload).json()['result']
+    return response
+
+def get_vmmap():
+    returnvalue = 0
+    blockcount = str(rpc("getblockcount", []))
+    returnvalue = rpc("vmmap", [blockcount, 0])
+    return returnvalue
+
+errors = False
 
 #TODO put a try, except around the special functions or direct in the subfunctions
-KEEP_DIRECTORYS = ["systeminfo", "version", "operatoraddresses", "listmasternodes", "vaultaggregation", "statistics"]
+KEEP_DIRECTORYS = ["systeminfo", "version", "operatoraddresses", "listmasternodes", "vaultaggregation", "statistics", "statistics_v2", "rpc_status", "getblockcountevm"]
 save_json_to_www(WWW_DIR, "systeminfo",        get_systeminfo())
 save_json_to_www(WWW_DIR, "version",           get_version(DEFID))
 save_json_to_www(WWW_DIR, "operatoraddresses", get_operators(DEFICONF))
-
-rpc_connection = create_connection_rpc(read_deficonf(DEFICONF))
+save_json_to_www(WWW_DIR, "getblockcountevm",  get_vmmap())
 
 try:
     functions = api_calls(API_LIST)
 except Exception as e:
     functions = {}
-    errors.append(f'api_calls() {str(e)}')
     print (f'api_calls() {str(e)}')
 
 if functions:
@@ -39,27 +58,24 @@ if functions:
         print (f'{i} {functions[i]}')
         info (f'{i} {functions[i]}')
         try:
-            if   len(functions[i]) == 0:
-                save_json_to_www(WWW_DIR, i, getattr(rpc_connection, i)())
-            elif len(functions[i]) == 1:
-                save_json_to_www(WWW_DIR, i, getattr(rpc_connection, i)(functions[i][0]))
-            elif len(functions[i]) == 2:
-                save_json_to_www(WWW_DIR, i, getattr(rpc_connection, i)(functions[i][0], functions[i][1]))
-            elif len(functions[i]) == 3:
-                save_json_to_www(WWW_DIR, i, getattr(rpc_connection, i)(functions[i][0], functions[i][1], functions[i][2]))
-        except Exception as e:
-            errors.append(f'{i} {str(e)}')
-            print (f'{i} {str(e)}')
-
-servername, ip_address = get_servername()
+            start = datetime.now()
+            save_json_to_www(WWW_DIR, i, rpc(i, functions[i]))
+            print (f"=> time: {datetime.now()-start}")
+            info (f"=> time: {datetime.now()-start}")
+        except Exception as err:
+            text = f"Problems with {filename} on {servername} {ip_address}\n{i} {err}"
+            print (text)
+            info (text)
+            if not errors:
+                get(f'https://api.telegram.org/{TELEGRAM_TOKEN}/sendMessage?chat_id={TELEGRAM_CHATID_ALARM}&text={text}')
+                errors = True
+else:
+    get(f'https://api.telegram.org/{TELEGRAM_TOKEN}/sendMessage?chat_id={TELEGRAM_CHATID_ALARM}&text=Nothing to collect.')
 
 print(f'removed directorys: {remove_unused_dirs(WWW_DIR, list(functions.keys()) + KEEP_DIRECTORYS)}')
 info(f'removed directorys: {remove_unused_dirs(WWW_DIR, list(functions.keys()) + KEEP_DIRECTORYS)}')
 
-if errors:
-    text = f"Problems with {filename} on {servername} {ip_address}\n{errors}"
-    print (text)
-    info (text)
-    get(f'https://api.telegram.org/{TELEGRAM_TOKEN}/sendMessage?chat_id={TELEGRAM_CHATID_ALARM}&text={text}')
+get(f'https://api.telegram.org/{TELEGRAM_TOKEN}/sendMessage?chat_id={TELEGRAM_CHATID_STATUS}&text=Runtime {filename} {servername} {ip_address}: {datetime.now()-starttime} ')
 
-info(f"End {filename}")
+info(f"End {filename} in {datetime.now()-starttime}")
+print(f"End {filename} in {datetime.now()-starttime}")
